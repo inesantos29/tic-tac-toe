68a688c696caba1af25454dccc5d776503c5d018
inesantos29 <ines.arts@hotmail.com>
2020-02-09 19:35:19 +0000

    Update components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Game } from './components/Game';
 
 function App(){
   return (
     <>
       <h1>Tic-tac-toe Game</h1>
+      <Game/>
     </>
   );
 }
